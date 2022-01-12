53c8db4de0642f9fd467e5cce197a5f09430ab04
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2022-01-12 12:08:58 +0100

    Added weapon fire visual effect

---
+++ Assets/Scripts/Weapons/WeaponEffect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Weapons
+{
+    public class WeaponEffect : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem fireVFX;
+
+        public void ShowFireEffect()
+        {
+            if (fireVFX is null)
+            {
+                Debug.LogError("Weapon does not have fire visual effect attached");
+                return;
+            }
+            
+            fireVFX.Play();
+        }
+    }
+}
